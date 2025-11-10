3c85aa7afda5a4b56034d00511f850cdc54b80af
Fábio Junio <fjosf1@aluno.ifal.edu.br>
2025-11-10 15:47:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-function takeFirstName(name){
-    var firstName = ''
-    const findSpace = name.indexOf(" ")
-    if(findSpace == -1){
-        firstName = name
-    }
-    else{
-        for(let i = 0; i < name.length; i++){
-            if(name[i] != name[findSpace]){
-                firstName += name[i]; 
+function takeName(){
+            var name = document.getElementById("completeName").value
+            var separatedName = []
+            var firstName = ''
+            var secondName = ''
+            const findSpace = name.indexOf(" ")
+            if(findSpace == -1){
+                separatedName.push(name)
+                return separatedName
             }
             else{
-                break;
-            }
+                for(let i = 0; i < name.length; i++){
+                    if(i < findSpace){
+                        firstName += name[i]; 
+                    }
+                    else if (i > findSpace){
+                        secondName += name[i]
+                    }
+                }
+            separatedName.push(firstName)
+            separatedName.push(secondName)
+            return separatedName
         }
     }
-    return document.getElementById("firstName").innerHTML(firstName)
+function getFirstName(){
+    document.getElementById('firstName').innerHTML = takeName()[0]
+}
+function getSecondName(name){
+    document.getElementById('secondName').innerHTML = takeName()[0]
 }
