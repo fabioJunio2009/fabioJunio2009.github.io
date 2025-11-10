df0403cdc1f2d2dc67e87d9e664de4f1390ef932
Fábio Junio <fjosf1@aluno.ifal.edu.br>
2025-11-10 16:02:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function takeName(){
-            var name = document.getElementById("completeName").value
-            var separatedName = []
-            var firstName = ''
-            var secondName = ''
-            const findSpace = name.indexOf(" ")
-            if(findSpace == -1){
-                separatedName.push(name)
-                return separatedName
-            }
-            else{
-                for(let i = 0; i < name.length; i++){
-                    if(i < findSpace){
-                        firstName += name[i]; 
-                    }
-                    else if (i > findSpace){
-                        secondName += name[i]
-                    }
-                }
-            separatedName.push(firstName)
-            separatedName.push(secondName)
-            return separatedName
-        }
+    var name = document.getElementById("completeName").value
+    var separatedName = []
+    var firstName = ''
+    var secondName = ''
+    const findSpace = name.indexOf(" ")
+    if(findSpace == -1){
+        separatedName.push(name)
+        return separatedName
     }
+    else{
+        for(let i = 0; i < name.length; i++){
+            if(i < findSpace){
+                firstName += name[i]; 
+            }
+            else if (i > findSpace){
+                secondName += name[i]
+            }
+        }
+        separatedName.push(firstName)
+        separatedName.push(secondName)
+        return separatedName
+    }
+}
 function getFirstName(){
     document.getElementById('firstName').innerHTML = takeName()[0]
 }
