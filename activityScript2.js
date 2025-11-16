9ea86ef3d773de91f30b9196b729114ffe7aeca5
Fábio Junio <fjosf1@aluno.ifal.edu.br>
2025-11-16 16:55:23 -0300

    Update activityScript2.js

--- activityScript2.js
+++ activityScript2.js
@@ -1,14 +1,26 @@
 function calculateIMC(){
+    // aqui pego os valores nos elementos com esses id abaixos
     const height = document.getElementById("height").value;
     const weight = document.getElementById("weight").value;
-    return Number(weight/height**2).toFixed(2)
-    
-    
+
+    // se a altura for menor ou igual a zero, incluindo um espaço vazio, ou o peso for menor ou igual a 0
+    // também incluindo um espaço vazio, retornará falso 
+    const imc = (height <= 0 || weight <= 0) ? false : Number(weight/height**2).toFixed(2)  
+    return imc  
 }
 
 function getIMC(){
-    const imc = calculateIMC(); 
-    const name = document.getElementById("name").value;
-    const message = `Olá ${name}. O seu IMC é de ${imc}.`;
-    document.getElementById("result").innerHTML = message
+    //pegando os dados
+    const imc = calculateIMC()
+    const name = document.getElementById("name").value
+    const Verifyname = (name === '' || Number(name) < 0) ? false : name
+    
+    //validação das informações obtidas 
+    if(imc === false || Verifyname === false ){
+        document.getElementById("result").innerHTML = 'Um ou mais campos está/estão inválido(s)'
+    }
+    else{
+        const message = `Olá, ${Verifyname}. O seu IMC é de ${imc}`
+         document.getElementById("result").innerHTML = message
+    }
 }
